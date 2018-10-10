2085a927ede51f2902a9dfe97413155b80a0166d
92718540206 <92718540206@JIPA-LABIN02-32.ifro.local>
2018-10-10 10:46:09 -0400

    Controlador Maker

--- calculadorajt/src/calculadorajt/FXMLDocumentController.java
+++ calculadorajt/src/calculadorajt/FXMLDocumentController.java
@@ -5,31 +5,84 @@
  */
 package calculadorajt;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author 92718540206
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private JFXButton btn7;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private JFXButton btn9;
+    @FXML
+    private JFXButton btn8;
+    @FXML
+    private JFXButton btn5;
+    @FXML
+    private JFXButton btn6;
+    @FXML
+    private JFXButton btn4;
+    @FXML
+    private JFXButton btn2;
+    @FXML
+    private JFXButton btn3;
+    @FXML
+    private JFXButton btn1;
+    @FXML
+    private JFXButton btnigual;
+    @FXML
+    private JFXButton btn0;
+    @FXML
+    private JFXButton btndividir;
+    @FXML
+    private JFXButton btnmultiplicar;
+    @FXML
+    private JFXButton btnsubtrair;
+    @FXML
+    private JFXButton btnsomar;
+    @FXML
+    private JFXButton btnlimpar;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void efetuar(ActionEvent event) {
+    }
+
+    @FXML
+    private void dividir(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void multiplicar(ActionEvent event) {
+    }
+
+    @FXML
+    private void subtrair(ActionEvent event) {
+    }
+
+    @FXML
+    private void somar(ActionEvent event) {
+    }
+
+    @FXML
+    private void limpar(ActionEvent event) {
+    }
     
 }
